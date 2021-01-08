d319c8d48ca09c46f31d18ba972a21c6390a40db
PPradhan222 <pradhan37202@gmail.com>
2021-01-09 01:20:04 +0530

    Show how much time ago any task has been created

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,29 @@ module ApplicationHelper
   def task_reporter(task)
     task.user_tasks.find_by(role: 'reporter').user.name
   end
+
+  def created_time_ago(task)
+    creation_time = task.created_at.utc.to_i
+    current_time = Time.now.utc.to_i
+    min_hr_day_year_ago(current_time - creation_time)
+  end
+
+  def min_hr_day_year_ago(time_difference)
+    case time_difference
+      when (0..59)
+        "just now"
+      when (60..3599)
+        pluralize_time(time_difference / 60, "minute")
+      when (3600..86399)
+        pluralize_time(time_difference / 3600, "hour")
+      when (86400..31535999)
+        pluralize_time(time_difference/ 86400, "day")
+      else
+        pluralize_time(time_difference/ 31536000, "year")
+    end  
+  end
+
+  def pluralize_time(amount, measure)
+    ActionController::Base.helpers.pluralize(amount, measure) + " ago"
+  end
 end
